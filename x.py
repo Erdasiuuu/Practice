65a4d7b54c47bca110b082d7e6297d65aa879552
Egor <elin4cod@gmail.com>
2024-11-11 03:20:26 +0700

    without progs

--- x.py
+++ x.py
@@ -1,18 +1,22 @@
 from PIL import Image, ImageTk
+import cv2
+import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
-def crop_image():
-    try:
-        left = int(entry_left.get())
-        top = int(entry_top.get())
-        right = int(entry_right.get())
-        bottom = int(entry_bottom.get())
-        
-        cropped_img = img.crop((left, top, right, bottom))
-        cropped_img.show()
-    except Exception as e:
-        print(f"Ошибка: {e}")
+img = None
+
+def capture_from_camera():
+    global img
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    if ret:
+        img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        img.show()
+    else:
+        print("Не удалось подключиться к камере")
+    cap.release()
+    cv2.destroyAllWindows()
 
 def load_image():
     global img
@@ -21,10 +25,23 @@ def load_image():
         img = Image.open(file_path)
         img.show()
 
+def crop_image():
+    if img:
+        try:
+            left = int(entry_left.get())
+            top = int(entry_top.get())
+            right = int(entry_right.get())
+            bottom = int(entry_bottom.get())
+            cropped_img = img.crop((left, top, right, bottom))
+            cropped_img.show()
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
 app = tk.Tk()
 app.title("Обрезка изображения")
 
 tk.Button(app, text="Загрузить изображение", command=load_image).pack()
+tk.Button(app, text="Сделать фото с камеры", command=capture_from_camera).pack()
 
 tk.Label(app, text="Координаты обрезки").pack()
 tk.Label(app, text="Левая:").pack()
