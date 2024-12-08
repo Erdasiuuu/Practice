36dc991268d292ade0f9f7843e46200b0accb7f2
Egor <elin4cod@gmail.com>
2024-12-08 23:52:42 +0700

    add dynamic button

--- main.py
+++ main.py
@@ -2,36 +2,45 @@ from PIL import Image, ImageTk
 import cv2
 import numpy as np
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import ttk
+from tkinter import filedialog, ttk
 
 
 class ImageEditor:
     def __init__(self):
-        window = None
-        image = None
+        self.root = None
+        self.window = None
+        self.image_frame = None
+        self.window = None
+        self.image = None
+        self.crop_frame = None
+        self.crop_frame_fields = None
         self.show_welcome_window()
 
 
     def show_welcome_window(self):
-        self.window = tk.Tk()
-        self.window.title("Загрузка изображения")
-        self.window.minsize(300, 50)
+        if self.root is None:
+            self.root = tk.Tk()
+            self.crop_frame = tk.Frame(self.root)
+        else:
+            self.clear_window()
 
-        tk.Button(self.window, text="Загрузить изображение", command=self.load_image).pack()
-        tk.Button(self.window, text="Сделать фото с камеры", command=self.capture_from_camera).pack()
-        tk.Button(self.window, text="Выйти", command=self.window.destroy).pack()
-        self.window.mainloop()
+        self.root.title("Загрузка изображения")
+        self.root.minsize(300, 50)
+
+        tk.Button(self.root, text="Загрузить изображение", command=self.load_image).pack()
+        tk.Button(self.root, text="Сделать фото с камеры", command=self.capture_from_camera).pack()
+        tk.Button(self.root, text="Выйти", command=self.root.destroy).pack()
+        self.root.mainloop()
 
 
     def load_image(self):
         file_path = filedialog.askopenfilename(
             title="Выберите изображение",
-            filetypes=[("Изображения", "*.png *.jpg *.jpeg *.bmp *.gif")],
+            filetypes=[("Изображения", "*.png *.jpg")],
         )
         if file_path:
-            self.image = Image.open(file_path)
-            self.show_image_window()
+            photo = Image.open(file_path)
+            self.show_image_window(photo)
 
 
     def capture_from_camera(self):
@@ -39,27 +48,47 @@ class ImageEditor:
         ret, frame = cap.read()
         cap.release()
         if ret:
-            self.image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            self.show_image_window()
+            photo = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            self.show_image_window(photo)
         else:
             print("Не удалось подключиться к камере")
         cv2.destroyAllWindows()
 
 
-    def show_image_window(self):
-        self.window.destroy()
-        self.window = tk.Tk()
-        self.window.title("Просмотр изображения")
+    def show_image_window(self, photo):
+        self.clear_window()
+        self.root.title("Просмотр изображения")
         
-        photo = ImageTk.PhotoImage(self.image)
+        self.image = ImageTk.PhotoImage(photo)
         
-        label = tk.Label(self.window, image=photo)
-        label.pack()    
+        label = tk.Label(self.root, image=self.image)
+        label.pack()
 
-        tk.Button(self.window, text="Обрезать изображение", command=self.show_crop_window).pack()
-        tk.Button(self.window, text="Выйти", command=self.window.destroy).pack()
-        window.mainloop()
+        tk.Button(self.crop_frame, text="Обрезать изображение", command=self.toggle_crop_frame).pack()
+        self.crop_frame.pack(fill=tk.BOTH)
+        tk.Button(self.root, text="Новое изображение", command=self.show_welcome_window).pack()
+        tk.Button(self.root, text="Выйти", command=self.root.destroy).pack()
 
+
+    def toggle_crop_frame(self):
+        """Показать или скрыть фрейм для ввода координат обрезки"""
+        if self.crop_frame_fields is None:
+            self.crop_frame_fields = tk.Frame(self.crop_frame)
+            tk.Label(self.crop_frame_fields, text="Координаты обрезки").pack()
+            tk.Label(self.crop_frame_fields, text="Левая:").pack()
+            tk.Entry(self.crop_frame_fields).pack()
+            tk.Label(self.crop_frame_fields, text="Верхняя:").pack()
+            tk.Entry(self.crop_frame_fields).pack()
+            tk.Label(self.crop_frame_fields, text="Правая:").pack()
+            tk.Entry(self.crop_frame_fields).pack()
+            tk.Label(self.crop_frame_fields, text="Нижняя:").pack()
+            tk.Entry(self.crop_frame_fields).pack()
+            self.crop_frame_fields.pack(fill=tk.BOTH)
+        else:
+            self.crop_frame_fields.destroy()
+            self.crop_frame_fields = None
+        
+        
 
     def show_crop_window(self):
         tk.Label(self.window, text="Координаты обрезки").pack()
@@ -77,17 +106,10 @@ class ImageEditor:
         entry_bottom.pack()
 
 
-    def crop_image(self):
-        if img:
-            try:
-                left = int(entry_left.get())
-                top = int(entry_top.get())
-                right = int(entry_right.get())
-                bottom = int(entry_bottom.get())
-                cropped_img = img.crop((left, top, right, bottom))
-                cropped_img.show()
-            except Exception as e:
-                print(f"Ошибка: {e}")
+    def clear_window(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        self.crop_frame = tk.Frame(self.root)
 
 
 if __name__ == '__main__':
