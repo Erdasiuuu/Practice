fa5718bcf32ef60abd4b999d163b8404857900ef
Elina Cody <elinacod@un-b4.kzn.21-school.ru>
2025-03-18 09:36:31 +0300

    tested

--- src/app.py
+++ src/app.py
@@ -4,9 +4,7 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.resizable(False, False)
-
         self.window_manager = WindowManager(self.root)
-
         self.switch_window("welcome")
 
     def switch_window(self, word):
@@ -19,5 +17,6 @@ class App:
         self.window_manager.current_window = word
         window = self.window_manager.get_window(
                                             word,
-                                            self.switch_window)
+                                            self.switch_window
+        )
         window.show()
--- src/color_channels.py
+++ src/color_channels.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import numpy as np
 from PIL import Image
 
